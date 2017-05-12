9b3f1baf37cc93aa2041ad20fd3851bfd65e8304
Songrui Huang <1023409568@qq.com>
2017-05-12 21:05:09 +0800

    商品信息类Commodity

---
+++ inc/Commodity.h
@@ -0,0 +1,93 @@
+//HSR20170509
+#ifndef __COMMODITY_H
+#define __COMMODITY_H
+
+#include "Unit.h"
+#include "Shop.h"
+#include "date.h"
+#include "SingleMoney.h"
+#include "Remark.h"
+#include<iostream>
+#include<string>
+
+class Commodity
+{
+    string _CommodityName;
+    SingleMoney _UnitPrice;
+    Unit _Unit;
+    Shop *pShop;
+    double _Quantity;
+    double _Discount;
+    Date _Date;
+    Remark *pRemark;
+public:
+
+    Commodity();
+
+    ~Commodity()=default;
+
+    void setCommodityName(string &_name)
+    {_CommodityName=_name;}
+
+    const string& CommodityName() const
+    {return _CommodityName;}
+
+    void setShop(Shop *_shop)
+    {pShop=_shop;}
+
+    const Shop& Shop() const
+    {return *pShop;}
+
+    void setQuantity(double _quantity)
+    {_Quantity=_quantity;}
+
+    double Quantity() const
+    {return _Quantity;}
+
+    void setDate(Date &_date)
+    {_Date=_date;}
+
+    const Date& Date() const
+    {return _Date;}
+
+    void setUnitPrice(SingleMoney &_total_price,double _quantity=1)
+    {_UnitPrice=_total_price/_quantity;}
+
+    const SingleMoney& UnitPrice() const
+    {return _UnitPrice;}
+
+    bool setDiscount(double discount)
+    {
+        if(discount>=0&&discount<1)
+        {_Discount=discount;return true;}
+        else
+        {return false;}
+    }
+    double Discount() const
+    {return _Discount;}
+
+    void setUnit(Unit &_unit)
+    {_Unit=_unit;}
+
+    bool convertToUnit(Unit &_unit)
+    {
+        if(_unit.getDimension()!=_Unit.getDimension())
+            return false;
+        else
+            _UnitPrice*=_Unit.convertTo(_unit.getUnit());
+    }
+
+    const Unit& Unit() const
+    {return _Unit;}
+
+    SingleMoney TotalPrice() const
+    {return _UnitPrice*_Quantity;}
+
+    SingleMoney OriginalUnitPrice() const
+    {return _UnitPrice/(1-_Discount);}
+
+    void setRemark(Remark *_remark)
+    {pRemark=_remark;}
+
+};
+#endif // __COMMODITY_H
---
+++ src/Commodity.cpp
@@ -0,0 +1,8 @@
+#include "../inc/Commodity.h"
+
+Commodity::Commodity():_CommodityName(),_UnitPrice(),\
+    _Unit(),pShop(nullptr),_Quantity(0),_Discount(0),_Date(),\
+    pRemark(nullptr)
+{
+
+}
