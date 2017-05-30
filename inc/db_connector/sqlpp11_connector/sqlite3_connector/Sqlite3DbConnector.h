dac8174a84800cbc94cc9dfb5c5b43738525e144
Songrui Huang <1023409568@qq.com>
2017-05-30 21:08:04 +0800

    数据库接口

---
+++ inc/db_connector/sqlpp11_connector/sqlite3_connector/Sqlite3DbConnector.h
@@ -0,0 +1,47 @@
+#ifndef __SQLITE3DBCONNECTOR_H
+#define __SQLITE3DBCONNECTOR_H
+
+#include <db_connector/AbstractDbConnector.h>
+
+#include <sqlpp11/custom_query.h>
+#include <sqlpp11/sqlite3/sqlite3.h>
+#include <sqlpp11/sqlpp11.h>
+#include <sqlite3.h>
+#include <vector>
+
+#include <db_connector/sqlpp11_connector/TabCommodity.h>
+
+namespace db_connector
+{
+    namespace sqlpp11
+    {
+        namespace sqlite3
+        {
+            namespace sql = sqlpp::sqlite3;
+            using sqlpp11_connector::TabCommodity;
+            class DbConnector : public AbstractDbConnector
+            {
+
+
+                sql::connection_config config;
+                sql::connection *db;
+                std::map<int,Commodity*> CommodityMap;
+                std::map<int,Shop*> ShopMap;
+                std::map<int,Remark*> RemarkMap;
+
+                const decltype(TabCommodity{}) tab_commodity=TabCommodity{};
+                Shop* selectShop(int ShopID);
+                Remark* selectRemark(int RemarkID);
+                Commodity* selectCommodity(int CommodityID);
+            public:
+                DbConnector(char *db_path);
+
+                std::vector<Commodity*>* selectCommodityAll();
+                std::vector<Commodity*>* selectCommodity(Filter *criteria);
+                void saveCommodity(Commodity *commodity);
+                void deleteCommodity(Commodity *commodity);
+            };
+        }
+    }
+}
+#endif // __SQLITE3DBCONNECTOR_H
---
+++ src/db_connector/sqlpp11_connector/sqlite3_connector/Sqlite3DbConnector.cpp
@@ -0,0 +1,125 @@
+#include <db_connector/sqlpp11_connector/sqlite3_connector/Sqlite3DbConnector.h>
+namespace db_connector
+{
+    namespace sqlpp11
+    {
+        namespace sqlite3
+        {
+            using std::vector;
+            using std::map;
+            DbConnector::DbConnector(char *db_path):config(),db(nullptr),CommodityMap(),ShopMap(),RemarkMap()
+            {
+                config.path_to_database=db_path;
+                config.flags=SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE;
+                config.debug=false;
+                db=new sql::connection(config);
+            }
+
+            vector<Commodity*>* DbConnector::selectCommodityAll()
+            {
+                vector<Commodity*>* ivec=new vector<Commodity*>();
+                Commodity* com;
+                for(const auto& row: (*db)(select(all_of(tab_commodity)).from(tab_commodity).unconditionally()))
+                {
+                    int id=row.tCommodityID;
+
+                    if(CommodityMap.find(id)==CommodityMap.end())
+                    {
+                        com=new Commodity(id);
+                        ivec->push_back(com);
+                        CommodityMap[id]=com;
+                        com->setCommodityName(std::string(row.tCommodityName));
+                        SingleMoney money(row.tUnitPrice,row.tCurrency);
+                        com->setUnitPrice(money);
+                        Unit unit((Unit::aUnit)((int)row.tUnit));
+                        com->setUnit(unit);
+                        Date d(row.tDate);
+                        com->setDate(d);
+                        //if(!row.tShopID==sqlpp::null)
+                        {
+
+                        }
+                        com->setQuantity(row.tQuantity);
+                        com->setDiscount(row.tDiscount);
+                        com->set_unmodified();
+                    }
+                    else
+                    {
+                        ivec->push_back(CommodityMap[id]);
+                    }
+
+                }
+                return ivec;
+            }
+
+            vector<Commodity*>* DbConnector::selectCommodity(Filter *criteria)
+            {
+
+            }
+
+            void DbConnector::saveCommodity(Commodity *com)
+            {
+                if(com->is_modified())
+                {
+                    int id=com->CommodityID();
+                    (*db)(update(tab_commodity).set(tab_commodity.tCommodityName=com->CommodityName(),\
+                                          tab_commodity.tUnitPrice=com->UnitPrice().Money(),\
+                                          tab_commodity.tCurrency=com->UnitPrice().Currency(),\
+                                          tab_commodity.tUnit=(int)(com->Unit().getUnit()),\
+                                          tab_commodity.tQuantity=com->Quantity(),\
+                                          tab_commodity.tDiscount=com->Discount(),\
+                                          tab_commodity.tDate=com->Date().toHour()\
+                                        ).where(tab_commodity.tCommodityID.in(id)));
+                }
+            }
+
+            void DbConnector::deleteCommodity(Commodity *commodity)
+            {
+
+            }
+
+            Commodity* DbConnector::selectCommodity(int CommodityID)
+            {
+                if(CommodityID<0) return nullptr;
+                if(CommodityMap.find(CommodityID)!=CommodityMap.end())
+                {
+                    return CommodityMap[CommodityID];
+                }
+                else
+                {
+                    Commodity* com=nullptr;
+                    for(const auto& row: (*db)(select(all_of(tab_commodity)).from(tab_commodity).where(tab_commodity.tCommodityID.in(CommodityID))))
+                    {
+                        com=new Commodity(CommodityID);
+                        CommodityMap[CommodityID]=com;
+                        com->setCommodityName(std::string(row.tCommodityName));
+                        SingleMoney money(row.tUnitPrice,row.tCurrency);
+                        com->setUnitPrice(money);
+                        Unit unit((Unit::aUnit)((int)row.tUnit));
+                        com->setUnit(unit);
+                        Date d(row.tDate);
+                        com->setDate(d);
+                        //if(!row.tShopID==sqlpp::null)
+                        {
+
+                        }
+                        com->setQuantity(row.tQuantity);
+                        com->setDiscount(row.tDiscount);
+                        com->set_unmodified();
+                    }
+                    return com;
+                }
+
+
+            }
+            Shop* selectShop(int ShopID)
+            {
+
+            }
+            Remark* selectRemark(int RemarkID)
+            {
+
+            }
+        }
+    }
+}
