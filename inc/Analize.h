e5189bc966299504af5d24640e21eed923e4b6a6
jkxing <973289085@qq.com>
2017-05-30 22:57:21 +0800

    jkxing

--- inc/Analize.h
+++ inc/Analize.h
@@ -1,53 +1,55 @@
 #pragma once
-#include "date.h"
 #include <vector>
+#include "date.h"
 #include "Commodity.h"
 #include "list.h"
-//#include "display.h"
-class Analize{
-	vector<List> list;
-	//Display _display;
-	Date _startDate, _endDate;
-	int _startHour, _endHour;
-	void init(int type, Date a, Date b, int startHour, int endHour, vector<string> s1, vector<string> s2, int l, int h) {
-		Date tmp = b;
-		_startDate = a;
-		_endDate = b;
-		_startHour = startHour;
-		_endHour = endHour;
-		switch (type) {
-		case 0:
-			for (; tmp >= a; tmp = tmp - TimeLen(0, 0, 1, 0))
-				list.push_back(List(tmp - TimeLen(0, 0, 1, 0), tmp, startHour, endHour, s1, s2, l, h));
-			break;
-		case 1:
-			for (; tmp >= a; tmp = tmp - TimeLen(0, 0, 7, 0))
-				list.push_back(List(tmp - TimeLen(0, 0, 7, 0), tmp, startHour, endHour, s1, s2, l, h));
-			break;
-		case 2:
-			for (; tmp >= a; tmp = tmp - TimeLen(0, 1, 0, 0))
-				list.push_back(List(tmp - TimeLen(0, 1, 0, 0), tmp, startHour, endHour, s1, s2, l, h));
-			break;
-		case 3:
-			for (; tmp >= a; tmp = tmp - TimeLen(1, 0, 0, 0))
-				list.push_back(List(tmp - TimeLen(1, 0, 0, 0), tmp, startHour, endHour, s1, s2, l, h));
-			break;
-		case 4:
-			list.push_back(List(a, b, startHour, endHour, s1, s2, l, h));
-		default:
-			list.push_back(List(a, b, startHour, endHour, s1, s2, l, h));
-			break;
-		}
-	}
+#include "Filter.h"
+#include "Display.h"
+class AnalizeVal {
+	double real, perc;
 public:
-	Analize(int type, Date a, Date b = Date(), int startHour = 0, int endHour = 24, vector<string> SelectCommoditiesNames = vector<string>(), vector<string> SelectShopsNames = vector<string>(), int lowPrice = 0, int highPrice = 100000000){
-		init(type, a, b, startHour, endHour, SelectCommoditiesNames, SelectShopsNames, lowPrice, highPrice);
-	}
-	Analize(int type, TimeLen a, Date b = Date(), int startHour = 0, int endHour = 24, vector<string> SelectCommoditiesNames = vector<string>(), vector<string> SelectShopsNames = vector<string>(), int lowPrice = 0, int highPrice = 100000000) {
-		init(type, b - a, b, startHour, endHour, SelectCommoditiesNames, SelectShopsNames, lowPrice, highPrice);
-	}
-	void display(int type)
-	{
-		//_display.show(type, list);
-	}
+	double getreal();
+	double getperc();
+	double setreal(double x);
+	double setperc(double x);
+};
+class Analize{
+public:
+	Filter *filter;
+	List* list;
+	map< string,AnalizeVal > mp;
+	DisplayImp* display;
+	void addDateStrict(Date a,Date b);
+	void addHourStrict(int st,int ed);
+	void addPriceStrict(int low,int high);
+	void addShopStrict(vector<string> shopnames);
+	void addCommodityStrict(vector<string> names);
+	virtual void startAnalize() = 0;
+	void show();
+};
+class AnalizeByDate :public Analize{
+public:
+	TimeLen* t;
+	AnalizeByDate(DisplayImp* display, TimeLen* t = new TimeLen(0,1,0,0));
+	void startAnalize();
+};
+class AnalizeByShop:public Analize{
+	vector<string> shopNames;
+	AnalizeByShop(DisplayImp* display, vector<string> s = vector<string>());
+	void startAnalize();
+};
+class AnalizeByPrice :public Analize {
+	int inter;
+	AnalizeByPrice(DisplayImp *display, int x = 0);
+	void startAnalize();
+};
+class AnalizeByHour :public Analize {
+	int inter;
+	AnalizeByHour(DisplayImp* display, int x = 0);
+	void startAnalize();
+};
+class AnalizeByComm :public Analize{
+	vector<string> commNames;
+	AnalizeByComm(DisplayImp* display, vector<string> s = vector<string>());
+	void startAnalize();
 };
