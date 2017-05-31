6c0f5acd8858b01f0d8909e336e9c1914ce06d57
Songrui Huang <1023409568@qq.com>
2017-05-31 14:16:15 +0800

    no message

--- src/test.cpp
+++ src/test.cpp
@@ -1,40 +1,79 @@
 #include "../inc/date.h"
 #include "../inc/Unit.h"
 #include "../inc/Shop.h"
+
+//#include "../inc/Analize.h"
+#include"../inc/User.h"
+#include"../inc/Display.h"
+
 #include "../inc/Analize.h"
+#include "../inc/Shop.h"
+
 #include <algorithm>
 #include <iostream>
 #include <cstring>
 #include <cstdio>
 #include <time.h>
+#include <sstream>
 using namespace std;
+AnalizeByDate* a;
 int main()
 {
-	Date a;
-	TimeLen b(0, 3, 29, 15);
-	cout << a << endl;
-	cout << b << endl;
-	cout << (a - b) << endl;
+   /* db_connector::AbstractDbConnector *a=new db_connector::sqlpp11::sqlite3::DbConnector("DataBase.db");
+    auto pp=a->selectCommodityAll();
+    pp=a->selectCommodityAll();
+    Date ndate(2017,8,9,23);
+    for(auto ss:(*pp))
+    {
+        std::cout<<(*ss)<<std::endl;
+        ss->setDate(ndate);
+    }
+
+    std::cout<<std::endl<<std::endl<<std::endl;
+    Commodity *comm=new Commodity();
+    comm->setCommodityName("rice");
+    SingleMoney money(8,"CNY");
+    comm->setUnitPrice(money);
+    Unit u(Unit::ch_Jing);
+    comm->setUnit(u);
+    comm->setQuantity(20);
+    comm->setDate(ndate);
+    pp->push_back(comm);
+    a->saveCommodity(*pp);*/
+
+	ExchangeRate exchange_rate;
+	string opr;
+	User* user = new User;
+	cin >> opr;
+	while (opr != "stop")
+	{
+		if (opr == "add_commodity")
+			user->addCommodity();
+		else if (opr == "add_list")
+			user->addList();
+		else if (opr == "add_paying_entry")
+		{
+			int i;
+			cin >> i;
+			user->addPayingEntry(i);
+		}
+		else if (opr == "add_multilist")
+			user->addMultiList();
+		else if (opr == "show_commodity")
+		{
+			int i;
+			cin >> i;
+			user->show("commodity", i);
+		}
+		else if (opr == "show_list")
+		{
+			int i;
+			cin >> i;
+			user->show("list", i);
+		}
+		cin >> opr;
+	}
+
 	system("pause");
-	/*double mass=1;
-	Unit mass_unit(Unit::kilogram);
-    cout<<mass<<mass_unit<<'=';
-    mass*=mass_unit.convertTo(Unit::ch_Liang);
-    cout<<mass<<mass_unit<<'=';
-    mass*=mass_unit.convertTo(Unit::pound);
-    cout<<mass<<mass_unit<<endl;
-    mass*=mass_unit.convertTo(Unit::kilogram);
-    mass*=mass_unit.convertTo(Unit::milliliter);
-    cout<<mass<<mass_unit<<endl;
-    mass*=mass_unit.convertTo(Unit::no_unit);
-    cout<<mass<<mass_unit<<endl;
-    mass_unit.setUnit(Unit::liter);
-    double &volume=mass;
-    cout<<volume<<mass_unit<<endl;
-    volume*=mass_unit.convertTo(Unit::us_gallon);
-    cout<<volume<<mass_unit<<endl;
-    volume*=mass_unit.convertTo(Unit::kilometer);
-    cout<<volume<<mass_unit<<endl;*/
-
-
+	return 0;
 }
