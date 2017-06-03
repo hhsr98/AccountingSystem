45703c33a0390c8d2f38232615efd30b0ab5afa9
Misty729 <13001279855@163.com>
2017-06-03 21:07:18 +0800

    完善商品信息类的命令行解析

--- src/ui/CommodityProxy.cpp
+++ src/ui/CommodityProxy.cpp
@@ -1,5 +1,5 @@
 #include "ui/CommodityProxy.h"
-
+#include<stdio.h>
 #include<sstream>
 using std::istringstream;
 AbstractProxy::state CommodityProxy::manipulate(std::string order, AbstractProxy *&new_proxy)
@@ -18,7 +18,67 @@ AbstractProxy::state CommodityProxy::manipulate(std::string order, AbstractProxy
                 if(temp1=="-n")
                 {com->setCommodityName(temp2);return AbstractProxy::done;}
                 if(temp1=="-q")
-                {}
+				{
+					double a;
+					std::stringstream stream;
+					//将string转换成double
+					stream<<temp2;
+					stream>>a;
+					com->setQuantity(a);
+					stream.clear(); 
+					return AbstractProxy::done;
+				}
+				if(temp1=="-m")
+				{
+					std::string a,b;
+					double a1;
+					a=temp2.substr(0,temp2.size()-3);//将数字拷贝到a中
+					std::stringstream stream;
+					stream<<a;
+					stream>>a1;//将字符串转化为double类型
+					b=temp2.substr(temp2.size()-3,3);
+					SingleMoney money(a1,b);//构造单币种金额类的新参数
+					com->setUnitPrice(money);//重置
+					return AbstractProxy::done;
+				}
+				if(temp1=="-o")
+				{
+					double temp;
+					std::stringstream stream;
+					//将string转换成double
+					stream<<temp2;
+					stream>>temp;
+					com->setDiscount(temp);
+					stream.clear(); 
+					return AbstractProxy::done;
+				}
+				if(temp1=="-d")//输入格式为年月日时，例如2017070718 为2017年7月7日18点
+				{
+					double y,m,d,h;
+					std::string y1,m1,d1,h1;
+					y1=temp2.substr(0,4);
+					m1=temp2.substr(4,2);
+					d1=temp2.substr(6,2);
+					h1=temp2.substr(8,2);
+					std::stringstream stream;
+					//将string转换成double
+					stream<<y1;
+					stream>>y;
+					stream.clear(); 
+					stream<<m1;
+					stream>>m;
+					stream.clear(); 
+					stream<<d1;
+					stream>>d;
+					stream.clear(); 
+					stream<<h1;
+					stream>>h;
+					stream.clear(); 
+					Date date(y,m,d,h);
+					com->setDate(date);
+					return AbstractProxy::done;
+				}
+
             }
             return AbstractProxy::fail;
         }
