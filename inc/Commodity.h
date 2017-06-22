527bb634249d2565ad5a5beb9a63fc52d520bbf7
Songrui Huang <1023409568@qq.com>
2017-06-22 17:26:14 +0800

    no message

--- inc/Commodity.h
+++ inc/Commodity.h
@@ -1,4 +1,13 @@
-//HSR20170509
+/*******************************************************
+名称：Commodity.h
+作者：黄松睿
+最后修改：2017-06-21
+内容描述：商品信息类的头文件。商品信息类保存商品的名称、
+          单价、数量、计量单位、商家、用户评论等信息，提
+          供一系列关于这些信息的操作。商品信息类与账单类
+          List间实现观察者模式，保证在商品信息被修改时包
+          含该商品的账单能进行相应判断。
+*******************************************************/
 #ifndef __COMMODITY_H
 #define __COMMODITY_H
 
@@ -13,10 +22,10 @@
 class List;
 class Commodity
 {
-    static int maxCommodityID;
+    static int maxCommodityID;      //当前最大的商品编号，用于对象持久化
 
-    bool modified;
-    int _CommodityID;
+    bool modified;                  //商品信息是否经过修改的标志位，用于对象持久化
+    int _CommodityID;               //商品编号，通常由数据库接口来设置，用户新添加的类ID为-1
     std::string _CommodityName;
     SingleMoney _UnitPrice;
     Unit _Unit;
@@ -24,18 +33,33 @@ class Commodity
     double _Quantity;
     double _Discount;
     Date _Date;
-    Remark *pRemark;
-    std::set<List*> regdit;
+    Remark *pRemark;                //用户评论接口
+    std::set<List*> regdit;         //观察者列表中包含该Commodity对象的所有List对象指针
 
     void inform_modified();
 public:
 
+    /*
+    *功能：构造器，对除_CommodityIDa外的所有成员进行默认初始化
+    *参数：
+    *   id=-1；商品信息类对象的编号，通常缺省，仅数据库接口应当对其赋值
+    */
     Commodity(int id=-1);
 
     ~Commodity();
 
+    /*
+    *功能：将一List对象添加进观察者列表中
+    *参数：
+    *   l：要添加的观察者
+    */
     void regObserverList(List *l);
 
+    /*
+    *功能：将一List对象从观察者列表中删除，若其不存在于列表中，不执行任何操作
+    *参数：
+    *   l：要删除的观察者
+    */
     void delObserverList(List *l);
 
     void set_unmodified()
